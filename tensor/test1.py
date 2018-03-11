1c9452fa5179fdecacfbd4d8ffb6dd9e0ddb94b8
young0428 <dudgb2380@naver.com>
2018-03-11 12:44:06 +0900

    tens

---
+++ tensor/test1.py
@@ -0,0 +1,48 @@
+import tensorflow as tf
+import numpy as np
+
+x_data = np.array([[0,0],[1,0],[1,1],[0,0],[0,0],[0,1]])
+y_data = np.array([[1,0,0],[0,1,0],[0,0,1],[1,0,0],[1,0,0],[0,0,1]])
+
+X = tf.placeholder(tf.float32)
+Y = tf.placeholder(tf.float32)
+
+W1 = tf.Variable(tf.random_uniform([2,10],-1,1))
+W2 = tf.Variable(tf.random_uniform([10,3],-1,1))
+
+b1 = tf.Variable(tf.zeros([10]))
+b2 = tf.Variable(tf.zeros([3]))
+
+
+
+L = tf.add(tf.matmul(X,W1),b1)
+L1 = tf.nn.relu(L)
+
+model = tf.add(tf.matmul(L1,W2),b2)
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=Y,logits=model))
+
+optimizer = tf.train.AdamOptimizer(learning_rate=0.01)
+train_op = optimizer.minimize(cost)
+
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
+print('----------Learning Start----------\n\n')
+
+for step in range(100):
+	sess.run(train_op,feed_dict={X:x_data, Y:y_data})
+
+	if (step+1)%10 == 0:
+		print(step+1, sess.run(cost, feed_dict={X: x_data, Y: y_data}))
+
+prediction = tf.argmax(model,axis=1)
+
+print(sess.run(prediction,feed_dict={X:x_data}))
+print(sess.run(tf.argmax(Y,axis=1),feed_dict={Y:y_data}))
+
+sess.close()
+print('----------done----------')
+
+
